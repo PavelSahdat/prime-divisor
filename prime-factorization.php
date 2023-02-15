ffe2e57a82df816a4e59c273511ce4bc4da0a453
pavel-sahdat <chymdpavel@gmail.com>
2023-02-15 23:55:31 +0600

    prime divisor problem successfully done 15-02-23

--- prime-factorization.php
+++ prime-factorization.php
@@ -1,15 +1,14 @@
 <?php
 
 //  Prime Factorization....
-// এবং count of the factor  অর্থাৎ 120 = 2^3 * 3^1 * 5^1 = count of the prime Divisor of 3 (ans)
 
 fscanf(STDIN,"%d",$n);
 $n2 = $n;
 $ans = 0;
     for($i = 2; $i <= $n2; $i++){
-     
+    
                  if($n2 % $i == 0 ){
-                        $tem++;
+                        $ans++;
                      $count= 0;
                        
                     while($n2 % $i == 0){ 
@@ -23,5 +22,6 @@ $ans = 0;
    
     }
     echo "Deference of prime  Factor in Total count Number of Prime Divisor ,$n  = ".$ans;
+    
+    ?>
 
-?>
